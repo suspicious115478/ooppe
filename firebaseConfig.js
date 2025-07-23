f75caa448aaf0fa74b12b7f2ccbba535303f7ece
suspicious115478 <sus24picious45@gmail.com>
2025-07-23 17:35:32 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,9 @@
 const admin = require('firebase-admin');
-
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.PROJECT_ID,
-  private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-  client_email: process.env.CLIENT_EMAIL,
-  token_uri: "https://oauth2.googleapis.com/token",
-};
+const serviceAccount = require('./serviceAccountKey.json'); // this is the JSON file
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.DATABASE_URL,
+  databaseURL: 'https://<your-project-id>.firebaseio.com' // update this
 });
 
 const db = admin.database();
