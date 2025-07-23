3a82fd7498f397b2f463713af6942d042a159e70
suspicious115478 <sus24picious45@gmail.com>
2025-07-23 17:38:56 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,7 @@ const serviceAccount = require('./serviceAccountKey.json'); // this is the JSON 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://<your-project-id>.firebaseio.com' // update this
+  databaseURL: https://project-6179641329587689976-default-rtdb.firebaseio.com/ // update this
 });
 
 const db = admin.database();
